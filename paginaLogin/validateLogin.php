d2e69248e2ed1142a6b90a04eb26e5294ef5abf0
Lucian <medrihanlucian@gmail.com>
2018-11-30 18:44:58 +0100

    aggiunte scritte aereo

---
+++ paginaLogin/validateLogin.php
@@ -0,0 +1,25 @@
+<html>
+    <head></head>
+    <body>
+        <?php
+
+            $dbconn = pg_connect("host=localhost port=5432 dbname=Progetto user=postgres password=lenzerini") or die('Could not connect: ' . pg_last_error());
+            if(!(isset($_POST['loginButton']))){
+                header("Location: ../index.html");
+            }
+            else{
+                $email = $_POST['inputEmail'];
+                $password = $_POST['inputPassword'];
+                $q1="select name from utente where email=$1 and password=$2";
+                $result=pg_query_params($dbconn,$q1,array($email,md5($password)));
+                if($line=pg_fetch_array($result,null,PGSQL_ASSOC)){
+                    $nome=$line['name'];
+                    header("Location: ../Welcome.php?name=$nome");
+                }
+                else{
+                    header("Location: TryAgain.html");
+                } 
+            }
+        ?>
+    </body>
+</html>
